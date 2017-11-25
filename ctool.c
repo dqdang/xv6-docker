b6dfb1f0fa796784536bb4e07b3b979c6780bb01
root <root@picklerick>
2017-11-25 23:32:58 +0000

    added

--- ctool.c
+++ ctool.c
@@ -2,9 +2,9 @@
 #include "types.h"
 #include "stat.h"
 #include "user.h"
+#include "defs.h"
 
-struct container
-{
+struct container{
   char *name;
   char **progs; 
   int max_proc;
@@ -14,60 +14,49 @@ struct container
   int used_disk;
 };
 
-struct
-{
-  struct container tuperwares[4];
+struct{
+  struct container tuperwares[NUM_VCS];
   int count;
 } ctable;
 
-void print_usage(int mode)
-{
-  if(mode == 0) // not enough arguments
-  {
+void print_usage(int mode){
+  if(mode == 0){ // not enough arguments
     printf(1, "Usage: ctool <mode> <args>\n");
   }
-  if(mode == 1) // create
-  {
+  if(mode == 1){ // create
     printf(1, "Usage: ctool create <max proc> <max mem> <max disk> <container> <exec1> <exec2> ...\n");
   }
-  if(mode == 2) // create with container created
-  {
+  if(mode == 2){ // create with container created
     printf(1, "Container taken. Failed to create, exiting...\n");
   }
-  if(mode == 3) // start
-  {
+  if(mode == 3){ // start
     printf(1, "Usage: ctool start <console> <container> <exec>\n");
   }
   
   exit();
 }
 
-int is_int(char c)
-{
+int is_int(char c){
   return c == '0' || c == '1' || c == '2' || c == '3' || c == '4' ||
          c == '5' || c == '6' || c == '7' || c == '8' || c == '9';
 }
 
 // ctool start vc0 c0 usfsh
-int start(int argc, char *argv[])
-{
+int start(int argc, char *argv[]){
   int id, fd;
   fd = open(argv[2], O_RDWR);
   printf(1, "fd = %d\n", fd);
-
   /* fork a child and exec argv[4] */
   id = fork();
 
-  if(id == 0)
-  {
+  if(id == 0){
     close(0);
     close(1);
     close(2);
     dup(fd);
     dup(fd);
     dup(fd);
-    if(chdir(argv[3]) < 0)
-    {
+    if(chdir(argv[3]) < 0){
       printf(1, "Container does not exist.");
       exit();
     }
@@ -79,6 +68,31 @@ int start(int argc, char *argv[])
   return 0;
 }
 
+int stop(char *argv[]){
+  // TODO: loop through processes and kill them
+}
+
+int delete(char *argv[]){
+  int id, i;
+  char rm[2];
+  id = fork();
+
+  //TODO: call pause
+
+  if(id == 0){
+    rm[0] = "rm";
+    rm[1] = argv[2];
+    exec(rm[0], rm);
+  }
+  for(i = 0; i < NUM_VCS; i++){
+    if(strcmp(tuperwares[i].name, argv[2]) == 0){
+      free(tuperwares[i]);
+      tuperwares[i] = NULL;
+      break;
+    }
+  }return 1;
+}
+
 // ctool create c0 8 8 8 cat ls echo sh ...
 int create(int argc, char *argv[])
 {
@@ -87,8 +101,7 @@ int create(int argc, char *argv[])
   mkdir[0] = "mkdir";
   mkdir[1] = argv[2];
 
-  while(!is_int(argv[2][cindex]))
-  {
+  while(!is_int(argv[2][cindex])){
     cindex = cindex + 1;
   }
 
@@ -101,14 +114,12 @@ int create(int argc, char *argv[])
   ctable.tuperwares[cindex].used_disk = 0;
 
   id = fork();
-  if(id == 0)
-  {
+  if(id == 0){
     exec(mkdir[0], mkdir);
   }
   id = wait();
 
-  for(i = 6; i < argc; i++) // going through ls echo cat ...
-  {
+  for(i = 6; i < argc; i++){ // going through ls echo cat ...
     char destination[32];
 
     strcpy(destination, "/");
@@ -120,16 +131,13 @@ int create(int argc, char *argv[])
     bytes = copy(argv[i], destination, ctable.tuperwares[cindex].used_disk, ctable.tuperwares[cindex].max_disk);
     printf(1, "Bytes for each file: %d\n", bytes);
 
-    if(bytes > 0)
-    {
+    if(bytes > 0){
       ctable.tuperwares[cindex].used_disk += bytes; 
     }
-    else
-    {
+    else{
       printf(1, "\nCONTAINER OUT OF MEMORY!\nFailed to copy executable %s. Removing incomplete binary.\n\n", argv[i]);
       id = fork();
-      if(id == 0)
-      {
+      if(id == 0){
         char *remove_args[2];
         remove_args[0] = "rm";
         remove_args[1] = destination;
@@ -137,40 +145,29 @@ int create(int argc, char *argv[])
       }
       id = wait();
     }
-  }
-  return 0;
+  }return 0;
 }
 
-int main(int argc, char *argv[])
-{
-  if(argc < 2)
-  {
+int main(int argc, char *argv[]){
+  if(argc < 2){
     print_usage(0);
   }
 
-  if(strcmp(argv[1], "create") == 0)
-  {
-    if(argc < 7)
-    {
+  if(strcmp(argv[1], "create") == 0){
+    if(argc < 7){
       print_usage(1);
     }
-    if(chdir(argv[2]) > 0)
-    {
+    if(chdir(argv[2]) > 0){
       print_usage(2);
     }
-
     create(argc, argv);
   }
 
-  if(strcmp(argv[1], "start") == 0)
-  {
-    if(argc < 5)
-    {
+  if(strcmp(argv[1], "start") == 0){
+    if(argc < 5){
       print_usage(3);
     }
-
     start(argc, argv);
   }
-
   exit();
 }
