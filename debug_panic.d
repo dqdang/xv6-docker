8a7a7060c121ec3f4ef721194b84a625c47e2c43
Derek Dang <dqdang17@gmail.com>
2017-11-27 20:38:59 -0800

    Clean repository

--- debug_panic.d
+++
@@ -1,4 +0,0 @@
-debug_panic.o: debug_panic.c fcntl.h \
- /opt/i386dev/lib/gcc/i386-xv6-elf/4.8.5/include/stdbool.h \
- /opt/i386dev/lib/gcc/i386-xv6-elf/4.8.5/include-fixed/limits.h types.h \
- user.h
