292f008121fe3ab93394f57030d8e8d55b9cbe57
ajtaggart <ajtaggart@dons.usfca.edu>
2017-11-30 23:10:04 +0000

    cleaned up free code

--- free.c
+++ free.c
@@ -24,40 +24,26 @@ int main(int argc, char *argv[]){
   char space2[128];
   char space3[128];
   char space4[128];
-
-
+  char percent[100];
+  char usedbytes[100];
+  char maxbytes[100];
+  char kallocs[100];
 
   if(strcmp(fs, "//") == 0){
-
-    char percent[100];
     itoa((getallusedmem()*4096)/(getallmaxmem()*4096), percent, 10);
-    char usedbytes[100];
     itoa((getallusedmem()*4096), usedbytes, 10);
-    char maxbytes[100];
     itoa((getallmaxmem()*4096), maxbytes, 10);
-    char kallocs[100];
     itoa (getallusedmem(), kallocs, 10);
-
-
-    printf(1, "\n%s\n", "Filesystem                  kallocs      Used       Available      Use%");
-    printf(1, "%s%s%s%s%s%s%s%s%s\n",fs, spaces(28-strlen(fs), space1), kallocs, spaces(13-strlen(kallocs), space2), usedbytes, spaces(11-strlen(usedbytes), space3), maxbytes, spaces(15-strlen(maxbytes), space4), percent);
   }
   else{
     int index = getactivefsindex();
-    char percent[100];
     itoa((getusedmem(index)*4096)/(getmaxmem(index)*4096), percent, 10);
-    char usedbytes[100];
     itoa((getusedmem(index)*4096), usedbytes, 10);
-    char maxbytes[100];
     itoa((getmaxmem(index)*4096), maxbytes, 10);
-    char kallocs[100];
     itoa (getusedmem(index), kallocs, 10);
-    printf(1, "%s\n", "Filesystem                  kallocs      Used       Available      Use%");
-    printf(1, "%s%s%s%s%s%s%s%s%s\n",fs, spaces(28-strlen(fs), space1), kallocs, spaces(13-strlen(kallocs), space2), usedbytes, spaces(11-strlen(usedbytes), space3), maxbytes, spaces(15-strlen(maxbytes), space4), percent);
-  
   }
-
-
+  printf(1, "%s\n", "Filesystem                  kallocs      Used       Available      Use%");
+  printf(1, "%s%s%s%s%s%s%s%s%s\n",fs, spaces(28-strlen(fs), space1), kallocs, spaces(13-strlen(kallocs), space2), usedbytes, spaces(11-strlen(usedbytes), space3), maxbytes, spaces(15-strlen(maxbytes), space4), percent);
   exit();
 
 }
