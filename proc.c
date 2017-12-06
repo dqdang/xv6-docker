cd9a02726269a7c5cb25be00f69140171351bf2d
ajtaggart <ajtaggart@dons.usfca.edu>
2017-12-06 12:05:43 +0000

    idk

--- proc.c
+++ proc.c
@@ -342,58 +342,11 @@ wait(void)
 //  - swtch to start running that process
 //  - eventually that process transfers control
 //      via swtch back to the scheduler.
-void
-scheduler(void)
-{
-  struct proc *p;
-  int i;
-  struct cpu *c = mycpu();
-  c->proc = 0;
-
-  for(;;){
-    // Enable interrupts on this processor.
-    sti();
-
-    // Loop over process table looking for process to run.
-    acquire(&ptable.lock);
-    for(i = -1; i < NUM_VCS; i++){
-      if(i == -1){
-        p = &ptable.proc[cabinet.next_proc];
-        setnextproc(i, nextvalidproc(i, cabinet.next_proc));
-      }
-      else{
-        if(cabinet.tuperwares[i].alive){
-          p = &ptable.proc[cabinet.tuperwares[i].next_proc];
-          setnextproc(i, nextvalidproc(i, cabinet.tuperwares[i].next_proc));
-        }else{
-          continue;
-        }
-      }
-
-  
-
-      // Switch to chosen process.  It is the process's job
-      // to release ptable.lock and then reacquire it
-      // before jumping back to us.
-      c->proc = p;
-      switchuvm(p);
-      p->state = RUNNING;
-
-      swtch(&(c->scheduler), p->context);
-      switchkvm();
-
-      // Process is done running for now.
-      // It should have changed its p->state before coming back.
-      c->proc = 0;
-    }
-    release(&ptable.lock);
-
-  }
-}
 // void
 // scheduler(void)
 // {
 //   struct proc *p;
+//   int i;
 //   struct cpu *c = mycpu();
 //   c->proc = 0;
 
@@ -403,9 +356,21 @@ scheduler(void)
 
 //     // Loop over process table looking for process to run.
 //     acquire(&ptable.lock);
-//     for(p = ptable.proc; p < &ptable.proc[NPROC]; p++){
-//       if(p->state != RUNNABLE)
-//         continue;
+//     for(i = -1; i < NUM_VCS; i++){
+//       if(i == -1){
+//         p = &ptable.proc[cabinet.next_proc];
+//         setnextproc(i, nextvalidproc(i, cabinet.next_proc));
+//       }
+//       else{
+//         if(cabinet.tuperwares[i].alive){
+//           p = &ptable.proc[cabinet.tuperwares[i].next_proc];
+//           setnextproc(i, nextvalidproc(i, cabinet.tuperwares[i].next_proc));
+//         }else{
+//           continue;
+//         }
+//       }
+
+  
 
 //       // Switch to chosen process.  It is the process's job
 //       // to release ptable.lock and then reacquire it
@@ -425,6 +390,41 @@ scheduler(void)
 
 //   }
 // }
+void
+scheduler(void)
+{
+  struct proc *p;
+  struct cpu *c = mycpu();
+  c->proc = 0;
+
+  for(;;){
+    // Enable interrupts on this processor.
+    sti();
+
+    // Loop over process table looking for process to run.
+    acquire(&ptable.lock);
+    for(p = ptable.proc; p < &ptable.proc[NPROC]; p++){
+      if(p->state != RUNNABLE)
+        continue;
+
+      // Switch to chosen process.  It is the process's job
+      // to release ptable.lock and then reacquire it
+      // before jumping back to us.
+      c->proc = p;
+      switchuvm(p);
+      p->state = RUNNING;
+
+      swtch(&(c->scheduler), p->context);
+      switchkvm();
+
+      // Process is done running for now.
+      // It should have changed its p->state before coming back.
+      c->proc = 0;
+    }
+    release(&ptable.lock);
+
+  }
+}
 
 int
 nextvalidproc(int cid, int index){
